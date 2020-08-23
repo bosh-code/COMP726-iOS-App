e6908aacca7d734102bee449e2748096f63177cb
Ryan Bosher <ryan.julian.bosher@icloud.com>
2020-08-24 10:18:57 +1200

    Initial Commit

---
+++ EggChain/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  EggChain
+//
+//  Created by Ryan Bosher on 24/08/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ EggChain/EggChainApp.swift
@@ -0,0 +1,17 @@
+//
+//  EggChainApp.swift
+//  EggChain
+//
+//  Created by Ryan Bosher on 24/08/20.
+//
+
+import SwiftUI
+
+@main
+struct EggChainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
