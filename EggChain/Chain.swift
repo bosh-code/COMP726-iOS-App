41596ca7c82245b9c7d330ddf66e68c150646402
Ryan Bosher <ryan.julian.bosher@icloud.com>
2020-10-21 16:07:51 +1300

    Added new chain struct for decoding the blockchain JSON coming in

---
+++ EggChain/Chain.swift
@@ -0,0 +1,29 @@
+import Foundation
+
+// MARK: - Welcome
+struct Chain: Codable {
+	let chain: [Block]?
+	let length: Int?
+}
+
+// MARK: - Chain
+struct Block: Codable {
+	let timestamp: Double?
+	let proof: Int?
+	let transactions: [Transactions]?
+	let previousHash: String?
+	let index: Int?
+
+	enum CodingKeys: String, CodingKey {
+		case timestamp, proof, transactions
+		case previousHash = "previous_hash"
+		case index
+	}
+}
+
+// MARK: - Transaction
+struct Transactions: Codable {
+	let amount: Int?
+	let recipient, sender, code, type: String?
+	let timestamp: Double?
+}
